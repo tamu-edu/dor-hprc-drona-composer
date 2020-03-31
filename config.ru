baa63719e6620588501259083b08dc353a4994e7
Tri Pham <phamminhtris@tamu.edu>
2020-03-31 17:03:46 -0500

    Add job controllers

--- config.ru
+++ config.ru
@@ -2,8 +2,10 @@ require 'sinatra'
 
 require './controllers/resources'
 require './controllers/requests'
+require './controllers/jobs'
 require './controllers/app'
 
 use ResourcesController
 use RequestsController
+use JobsController
 run Sinatra::Application
--- controllers/jobs.rb
+++ controllers/jobs.rb
@@ -1,6 +1,10 @@
 class JobsController < Sinatra::Base
     get '/jobs' do
-        
+        squeue = Squeue.new
+        jobs, squeue_error = squeue.exec
+
+        json_jobs = jobs.map { |o| Hash[o.each_pair.to_a] }
+        {'data' => json_jobs }.to_json
     end
 
 end
