36ba54eaddd06af7b8beb18df06737f81d4bf024
Tri Pham <phamminhtris@tamu.edu>
2020-04-06 17:03:03 -0500

    Use new driver for util chart

--- machine_driver_scripts/utilization.sh
+++ machine_driver_scripts/utilization.sh
@@ -4,12 +4,12 @@
 allocated_nodes=$(/sw/local/bin/pestat -s alloc | tail -n+4 | wc -l)
 mixed_nodes=$(/sw/local/bin/pestat -s down | tail -n+4 | wc -l)
 idle_nodes=$(/sw/local/bin/pestat -s idle | tail -n+4 | wc -l)
-NODE_DATA="nodes:  { allocated: '$allocated_nodes', mixed:  '$mixed_nodes', idle: '$idle_nodes' }"
+NODE_DATA='"nodes":  { "allocated": '$allocated_nodes', "mixed":  '$mixed_nodes', "idle": '$idle_nodes' }'
 
 allocated_cpus=$(/sw/local/bin/pestat -s alloc,mix,idle | tail -n+4 | awk '{print $4}' | awk 'NR>3' | awk '{s+=$1} END {printf "%.0f", s}')
 total_cpus=$(/sw/local/bin/pestat -s alloc,mix,idle | tail -n+4 | awk '{print $5}' | awk 'NR>3' | awk '{s+=$1} END {printf "%.0f", s}')
 idle_cpus=$(expr $total_cpus - $allocated_cpus)
-CORE_DATA="cores: { allocated: '$allocated_cpus', idle: '$idle_cpus'}"
+CORE_DATA='"cores": { "allocated": '$allocated_cpus', "idle": '$idle_cpus'}'
 
-UTIL_DATA="{ data: { $NODE_DATA, $CORE_DATA }}"
+UTIL_DATA='{ "data": { '$NODE_DATA', '$CORE_DATA' }}'
 echo $UTIL_DATA
