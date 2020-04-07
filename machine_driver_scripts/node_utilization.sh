98d6a7ac6d0234045072ac7a3e1b55a3b2f159e1
Tri Pham <phamminhtris@tamu.edu>
2020-04-07 13:08:59 -0500

    Compact quota table

--- machine_driver_scripts/node_utilization.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-MIX=$(/sw/local/bin/pestat -s mix | wc -l)
-ALLOC=$(/sw/local/bin/pestat -s alloc | wc -l)
-IDLE=$(/sw/local/bin/pestat -s idle | wc -l)
-echo "$ALLOC $MIX $IDLE"
--- public/custom/js/quota.js
+++ public/custom/js/quota.js
@@ -1,4 +1,3 @@
-
 function colorize_percentage_value(percent) {
   if (percent < 30.0) {
     return `${percent} %`.fontcolor('green');
@@ -23,93 +22,100 @@ function formatBytes(bytes, decimals = 2) {
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
 
-function formatKBytes(data, type, row) {
+function formatKBytes(data) {
   bytes = data * 1024;
   return formatBytes(bytes);
 }
 
 
 function populate_quota() {
-    var quota_table = document.querySelector("#quota_table");
+  var quota_table = document.querySelector("#quota_table");
 
-    $('#quota_table').DataTable({
-        "paging": false,
-        "searching": false,
-        "info": false,
-        "ordering": false,
-        "scrolling": false,
-        ajax: {
-            dataType: "json",
-            url: document.dashboard_url + '/resources/disk/quota',
-            method: "GET",
-        },
-        "columns": [
-            { "data": "name" },
-            { "data": "disk_usage", render: formatKBytes },
-            { "data": "disk_limit", render: formatKBytes },
-            {
-              "data": null, render: function (data, type, row) {
-                  percent = (row.disk_usage / row.disk_limit) * 100
-                  return `${colorize_percentage_value(percent.toFixed(2))}`;
-              }
-            },
-            { "data": "file_usage"},
-            { "data": "file_limit"},
-            {
-                "data": null, render: function (data, type, row) {
-                    percent = (row.file_usage / row.file_limit) * 100
-                    return `${colorize_percentage_value(percent.toFixed(2))}`;
-                }
-            },
-        ]
-    });
+  $('#quota_table').DataTable({
+    "paging": false,
+    "searching": false,
+    "info": false,
+    "ordering": false,
+    "scrolling": false,
+    ajax: {
+      dataType: "json",
+      url: document.dashboard_url + '/resources/disk/quota',
+      method: "GET",
+    },
+    "columns": [{
+        "data": "name"
+      },
+      {
+        "data": "disk_usage", "sClass":  "text-right",
+        render: function (data, type, row) {
+          percent = (row.disk_usage / row.disk_limit) * 100
+          return `${formatKBytes(data)} (${colorize_percentage_value(percent.toFixed(2))})`;
+        }
+      },
+      {
+        "data": "disk_limit", "sClass":  "text-right",
+        render: formatKBytes
+      },
+
+      {
+        "data": "file_usage", "sClass":  "text-right",
+        render: function (data, type, row) {
+          percent = (row.file_usage / row.file_limit) * 100
+          return `${data} (${colorize_percentage_value(percent.toFixed(2))})`;
+        }
+      },
+      {
+        "data": "file_limit", "sClass":  "text-right",
+      },
+    ]
+  });
 }
 
 function setup_quota_request_sender(request_endpoint, form_id, modal_id) {
-    // Source: https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_forms_through_JavaScript
-    window.addEventListener("load", function () {
-      function sendData() {
-        const XHR = new XMLHttpRequest();
-  
-        // Bind the FormData object and the form element
-        const FD = new FormData(form);
-  
-        // Define what happens on successful data submission
-        XHR.addEventListener("load", function (event) {
-          alert(event.target.responseText);
-        });
-  
-        // Define what happens in case of error
-        XHR.addEventListener("error", function (event) {
-          alert(`Oops! ${event.target.responseText}`);
-        });
-  
-        // Set up our request
-        XHR.open("POST", request_endpoint);
-  
-        // The data sent is what the user provided in the form
-        XHR.send(FD);
-      }
-  
-      // Access the form element...
-      let form = document.getElementById(form_id);
-  
-      // ...and take over its submit event.
-      form.addEventListener("submit", function (event) {
-        event.preventDefault();
-  
-        sendData();
+  // Source: https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_forms_through_JavaScript
+  window.addEventListener("load", function () {
+    function sendData() {
+      const XHR = new XMLHttpRequest();
 
-        // dismiss our modal
-        $(modal_id).modal('hide');
+      // Bind the FormData object and the form element
+      const FD = new FormData(form);
+
+      // Define what happens on successful data submission
+      XHR.addEventListener("load", function (event) {
+        alert(event.target.responseText);
       });
+
+      // Define what happens in case of error
+      XHR.addEventListener("error", function (event) {
+        alert(`Oops! ${event.target.responseText}`);
+      });
+
+      // Set up our request
+      XHR.open("POST", request_endpoint);
+
+      // The data sent is what the user provided in the form
+      XHR.send(FD);
+    }
+
+    // Access the form element...
+    let form = document.getElementById(form_id);
+
+    // ...and take over its submit event.
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      sendData();
+
+      // dismiss our modal
+      $(modal_id).modal('hide');
     });
-  }
+  });
+}
 
 (() => {
-    quota_request_endpoint = document.dashboard_url + "/request/quota";
+  quota_request_endpoint = document.dashboard_url + "/request/quota";
 
-    populate_quota();
-    setup_quota_request_sender(quota_request_endpoint, "modalQuotaRequestForm", "#requestQuotaModal");
+  populate_quota();
+  setup_quota_request_sender(quota_request_endpoint, "modalQuotaRequestForm", "#requestQuotaModal");
 
 })()
