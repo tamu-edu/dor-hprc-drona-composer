dd150b4ac32212e2059d5be20a9ca207a090e345
Tri Pham <phamminhtris@tamu.edu>
2020-04-07 12:20:24 -0500

    Add new driver script for quota

--- machine_driver_scripts/showquota_ood.sh
+++ machine_driver_scripts/showquota_ood.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-mount | grep -qw general &&   mmlsquota -u $USER general:root | tail -n 1 | awk '{printf "%10s %10s\n", $4, $6}'
-mount | grep -qw scratch && mmlsquota -u $USER scratch:root | tail -n 1 | awk '{printf "%10s %10s\n", $4, $6}'
+#  below is the header
+#  disk_name      disk_usage(K)    disk_limit(k)    file_usage      file_limit"
+function parse_disk() {
+    DATA_STRING=$1
+    IFS=',' read -ra DISK_DATA <<< "$DATA_STRING";
+    echo '{"name": "'${DISK_DATA[0]}'", "disk_usage": '${DISK_DATA[1]}', "disk_limit": '${DISK_DATA[2]}', "file_usage": '${DISK_DATA[3]}', "file_limit": '${DISK_DATA[4]}'}'
+}
+
+HOME_QUOTA=$(mount | grep -qw general && mmlsquota --block-size 1K -u $USER general:root | tail -n 1);
+HOME_QUOTA=$(awk '{printf "%-10s, %10s, %10s, %10s, %10s\n", "/home", $4, $6, $10, $12}' <<< $HOME_QUOTA);
+HOME_QUOTA=$(parse_disk "${HOME_QUOTA}");
+
+SCRATCH_QUOTA=$(mount | grep -qw scratch && mmlsquota --block-size 1K -u $USER scratch:root | tail -n 1);
+SCRATCH_QUOTA=$(awk '{printf "%-10s, %10s, %10s, %10s, %10s\n", "/scratch", $4, $6, $10, $12}' <<< $SCRATCH_QUOTA);
+SCRATCH_QUOTA=$(parse_disk "${SCRATCH_QUOTA}");
+
+OUT='{ "disks": ['${HOME_QUOTA}', '${SCRATCH_QUOTA}']}'
+echo $OUT
