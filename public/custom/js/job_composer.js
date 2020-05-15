28ad2b2fc2545132de1ced6e53ceeeebce99a880
Tri Pham <phamminhtris@tamu.edu>
2020-05-15 13:56:45 -0500

    Move runtime before script upload

--- public/custom/js/job_composer.js
+++ public/custom/js/job_composer.js
@@ -102,8 +102,71 @@ function register_on_file_changed_listener() {
             return;
         } else {
             runtime_config.style.display = "block";
+            update_run_command();
         }
+    };
+}
+
+function set_run_command(command) {
+    var run_cmd_input = document.getElementById('run_command');
+
+    if (run_cmd_input == null) {
+        return;
     }
+
+    run_cmd_input.value = command;
+}
+
+function set_run_command_placeholder(message) {
+    var run_cmd_input = document.getElementById('run_command');
+
+    if (run_cmd_input == null) {
+        return;
+    }
+    run_cmd_input.value = "";
+    run_cmd_input.placeholder = message;
+}
+
+function update_run_command() {
+    var runtime_env_selector = document.getElementById("runtime_env");
+    if (runtime_env_selector == null) {
+        return;
+    }
+
+    let file_picker = document.getElementById('executable_file_input');
+    if (file_picker == null || file_picker.files.length === 0) {
+        return;
+    }
+    let file_name = file_picker.files.item(0).name;
+
+    let runtime = runtime_env_selector.value;
+    switch (runtime) {
+        case 'shell':
+            set_run_command(`./${file_name}`);
+            break;
+        case 'python': 
+            set_run_command(`python ${file_name}`);
+            break;
+        case 'matlab':
+            set_run_command(`matlab ${file_name}`);
+            
+            break;
+        case 'other':
+            // console.log("Other");
+            set_run_command_placeholder(`Please enter your run command. Use `);
+            break;
+        default:
+            console.error("Runtime nort supported error.");
+    }
+}
+
+function register_on_runtime_change_listener() {
+    var runtime_env_selector = document.getElementById("runtime_env");
+    if (runtime_env_selector == null) {
+        return;
+    }
+
+    runtime_env_selector.onchange = update_run_command;
 }
 
 
@@ -113,4 +176,5 @@ function register_on_file_changed_listener() {
     register_add_module_handler();
     register_slurm_submit_button();
     register_on_file_changed_listener();
+    register_on_runtime_change_listener();
 })();
