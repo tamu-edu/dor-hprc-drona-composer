982b45b0245057dba025721feaf4700fbe40613c
Phi Thuan Au <thau@tamu.edu>
2021-03-17 16:51:05 -0500

    Remove mock script, setup_env.sh

--- setup_env.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-ml load Ruby/2.6.3-GCCcore-8.2.0
-ml load git/2.21.0-GCCcore-8.2.0-nodocs
-export GEM_HOME="/var/www/ood/apps/dev/phamminhtris/gateway/dashboard/vendor/bundle"
-
-# /var/log/ondemand-nginx/phamminhtris
-
