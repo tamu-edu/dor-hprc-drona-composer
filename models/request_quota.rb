8c4d1c7fa8f2a3e8b88dc543be6f56284c4ec9e3
Phi Thuan Au <thau@tamu.edu>
2021-05-14 13:15:44 -0500

    Modify email format for quota request.

--- models/request_quota.rb
+++ models/request_quota.rb
@@ -1,28 +1,30 @@
 require 'open3'
 
 class QuotaRequest
-
-    def compose_email(user, cluster_name, file_limit, disk_space, justification)
-        body =  "User: #{user}\n" \
-                "Cluster: #{cluster_name}\n" \
-                "File limit: #{file_limit}\n"\
-                "Disk space: #{disk_space}\n" \
-                "Justification: #{justification}"
-        
-        body = body.strip
-        body
-    end
-
     def generate_email(params)
-      justfication = params[:request_justification]
-      file_limit = params[:total_file_limit]
-      disk_space = params[:desired_disk]
+      
       cluster_name = params[:cluster_name]
       user = ENV["USER"]
 
-      subject = "[#{cluster_name}] Quota Request: #{user}"
-      body = compose_email(user, cluster_name, file_limit, disk_space, justfication)
+      current_quota = params[:current_quota]
+      current_file_limit = params[:current_file_limit]
 
+      disk_space = params[:desired_disk]
+      file_limit = params[:total_file_limit]
+      justification = params[:request_justification]
+      
+      subject = "[#{cluster_name}] Quota Request: #{user}"
+      body =  "Cluster: #{cluster_name}\n" \
+                "User: #{user}\n" \
+                "------------------------------------------------------------\n" \
+                "Current disk space: #{current_quota}\n" \
+                "Current file limit: #{current_file_limit}\n" \
+                "------------------------------------------------------------\n" \
+                "Requesting disk space: #{disk_space}\n" \
+                "Requesting file limit: #{file_limit}\n" \
+                "Justification: #{justification}"
+        
+      body = body.strip
       [subject, body]
     end
 end
