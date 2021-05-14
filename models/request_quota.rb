350328de42bc1ac8b243fe22ac8ccb0e1699bcf8
Phi Thuan Au <thau@tamu.edu>
2021-05-14 11:59:18 -0500

    Add clustername and netid to quota request subject

--- models/request_quota.rb
+++ models/request_quota.rb
@@ -2,8 +2,7 @@ require 'open3'
 
 class QuotaRequest
 
-    def compose_email(cluster_name, file_limit, disk_space, justification)
-        user = ENV["USER"]
+    def compose_email(user, cluster_name, file_limit, disk_space, justification)
         body =  "User: #{user}\n" \
                 "Cluster: #{cluster_name}\n" \
                 "File limit: #{file_limit}\n"\
@@ -19,9 +18,10 @@ class QuotaRequest
       file_limit = params[:total_file_limit]
       disk_space = params[:desired_disk]
       cluster_name = params[:cluster_name]
+      user = ENV["USER"]
 
-      subject = "QuotaReq"
-      body = compose_email(cluster_name, file_limit, disk_space, justfication)
+      subject = "[#{cluster_name}] Quota Request: #{user}"
+      body = compose_email(user, cluster_name, file_limit, disk_space, justfication)
 
       [subject, body]
     end
