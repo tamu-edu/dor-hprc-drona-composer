cac6bf1aff3c9f27aa7bfbeb00864d06d3298464
Duy Duc Hai Pham <duy@mgt3.cluster>
2023-11-28 15:01:43 -0600

    update modularize script for environments

--- modularize.sh
+++ modularize.sh
@@ -8,20 +8,20 @@ echo "."
 mkdir jobwizzard 
 mkdir jobwizzard/controllers jobwizzard/views jobwizzard/machine_driver_scripts
 
-cp -r templates/ jobwizzard/
+# cp -r templates/ jobwizzard/
 cp -r public/custom/js/ jobwizzard/
-cp -r schemas/ jobwizzard/
+# cp -r schemas/ jobwizzard/
 cp -r templates-all jobwizzard/
-cp -r maps/ jobwizzard/
+# cp -r maps/ jobwizzard/
+cp -r environments/ jobwizzard/
 
 cp controllers/job_composer.rb jobwizzard/controllers/job_composer.rb
 cp views/_job_config_form.erb jobwizzard/views/_job_config_form.erb
 cp machine_driver_scripts/engine jobwizzard/machine_driver_scripts/engine
+cp machine_driver_scripts/utils.py jobwizzard/machine_driver_scripts/utils.py
 
-echo "mkdir -p ../templates ../schemas ../templates-all ../maps
-cp -r templates/. ../templates
-cp -r schemas/. ../schemas
-cp -r maps/. ../maps
+echo "mkdir -p ../templates-all ../environments
+cp -r environments/. ../environments
 cp -r templates-all/. ../templates-all
 cp -r machine_driver_scripts/. ../machine_driver_scripts/
 cp -r  controllers/. ../controllers/
