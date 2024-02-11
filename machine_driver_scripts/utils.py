b15760a08f5e7af8498864e149a2eb455c2c9939
Marinus Pennings <pennings@tamu.edu>
2024-02-11 00:10:33 -0600

    update utils.py, adding env scripts

--- machine_driver_scripts/utils.py
+++ machine_driver_scripts/utils.py
@@ -1,5 +1,34 @@
+import os
+
 def retrieve_workers(workers, default):
     if workers:
         return f"-w {workers}"
     else:
         return f"-w {default}"
+
+def retrieve_R_version(version):
+    if version == "4.2.2":
+        return f"module load GCC/12.2.0  OpenMPI/4.1.4 R_tamu/4.2.2"
+    else:
+        return f""
+
+def retrieve_R_driver(mpistring):
+    if mpistring == "mpi":
+        return f"mpirun -np 1 "
+    else:
+        return f""
+
+def retrieve_alphapickle(pickle, proteinfasta, outputdir):
+    if pickle == "true":
+       name = os.path.splitext(os.path.basename(proteinfasta))[0]
+       return f"run_AlphaPickle.py   -od " +  outputdir + "/" + name
+    else:
+       return f""
+
+def retrieve_loaded_modules(modules):
+    if modules == "":
+        return f""
+    else:
+        return "module load foss/2023a " + modules
+
+
