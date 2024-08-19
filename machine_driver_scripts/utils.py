bad05690d97139a88003c9d362046f38ef1129ae
Andrii Kryvenko <a11155@portal.cluster>
2024-08-19 13:43:03 -0500

    TD-82 local utils.py functions can access global utils.py functions

--- machine_driver_scripts/utils.py
+++ machine_driver_scripts/utils.py
@@ -106,18 +106,6 @@ def retrieve_mopts(workers,threads,walltime,memory,extra_params):
         options_string="-x '" + additional + "' " + options_string
     return f"" + options_string
 
-def check_my_code(job_name, cores, memory):
-    warnings = []
-    if memory == "":
-        drona_add_warning(job_name, "The memory is unspecified")
-    if cores == "":
-        drona_add_warning(job_name, "The number of cores are unspecified")
-
-    return ""
-def drona_add_additional_files(job_name, cores):
-    drona_add_additional_file(job_name, "testing_dynamic.txt")
-    if cores == "1":
-        drona_add_additional_file(job_name, "testing_dynamic2.txt")
 
 def drona_add_additional_file(job_name, additional_file):
     additional_files_path = os.path.join("/tmp", f"{job_name}.additional_files")
