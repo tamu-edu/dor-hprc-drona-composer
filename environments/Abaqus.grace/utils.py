45d1f7c24fd915988d60fc2b5e96aa2f4519c9ee
Andrii Kryvenko <a11155@portal.cluster>
2024-09-20 13:49:04 -0500

    Example in Abaqus.grace Improved

--- environments/Abaqus.grace/utils.py
+++ environments/Abaqus.grace/utils.py
@@ -6,7 +6,7 @@ def check_my_code(job_name, cores, memory):
         drona_add_warning("The number of cores are unspecified")
     return ""
 def drona_add_additional_files(job_name, cores):
-    drona_add_additional_file("testing_dynamic.txt", preview_order=2, preview_name="dynamic_order2")
+    drona_add_additional_file("testing_dynamic.txt", preview_order=2, preview_name="dynamic_file_order_2")
     if cores == "1":
-        drona_add_additional_file("testing_dynamic2.txt", preview_name="dynamic_order0")
+        drona_add_additional_file("testing_dynamic2.txt", preview_name="dynamic_file_order_0")
 
