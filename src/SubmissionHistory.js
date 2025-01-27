2c3422e0085b7966f3b630bb08f788e213039f01
Andrii Kryvenko <a11155@portal.cluster>
2025-01-27 16:03:21 -0600

    Added production build functionality

--- src/SubmissionHistory.js
+++ src/SubmissionHistory.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import DataTable from 'react-data-table-component';
-import { tableCustomStyles } from '../static/custom/css/tablestyle.jsx';
+import { tableCustomStyles } from './tablestyle.jsx';
 
 const SubmissionHistory = ({ isExpanded, handleRerun, handleForm }) => {
   const [jobHistory, setJobHistory] = useState([]);
