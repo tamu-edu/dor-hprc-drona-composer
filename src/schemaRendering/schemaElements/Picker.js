823dc6cdd41170a877209c71e10de8a650354c9f
Andrii Kryvenko <a11155@mgt3.cluster>
2025-02-17 14:23:57 -0600

    Drona Composer added to the dashboard

--- src/schemaRendering/schemaElements/Picker.js
+++ src/schemaRendering/schemaElements/Picker.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useState, useRef, useContext } from "react";
-import { GlobalFilesContext } from "../../index.js";
+import { GlobalFilesContext } from "../../ComposerWrapper.js";
 import FormElementWrapper from "../utils/FormElementWrapper"
 
 function Picker(props) {
--- src/schemaRendering/schemaElements/Uploader.js
+++ src/schemaRendering/schemaElements/Uploader.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef, useContext } from "react";
-import { GlobalFilesContext } from "../../index.js";
+import { GlobalFilesContext } from "../../ComposerWrapper.js";
 import FormElementWrapper from "../utils/FormElementWrapper";
 
 function Uploader(props) {
