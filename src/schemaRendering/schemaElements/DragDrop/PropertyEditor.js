03764284d82ea7df3dd70676c38d05ba361c2a16
Andrii Kryvenko <a11155@mgt3.cluster>
2025-06-10 11:14:12 -0500

    Drag Drop UI enhanced

--- src/schemaRendering/schemaElements/DragDrop/PropertyEditor.js
+++ src/schemaRendering/schemaElements/DragDrop/PropertyEditor.js
@@ -35,7 +35,7 @@ function PropertyField({ property, value, onChange, propertyKey }) {
 
     case "checkbox":
       return (
-        <div style={{ display: "flex", alignItems: "center", marginLeft: "20px", gap: "0.5rem" }}>
+        <div style={{ position: "relative", display: "flex", alignItems: "center", marginLeft: "20px", gap: "0.5rem" }}>
           <input
             type="checkbox"
             checked={value !== undefined ? value : defaultValue || false}
@@ -110,19 +110,28 @@ function PropertyEditor({ element, template, onSave, onCancel }) {
     onSave(values);
   };
 
+  const handleBackdropClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onCancel();
+    }
+  };
+
   return (
-    <div style={{
-      position: "fixed",
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      backgroundColor: "rgba(0, 0, 0, 0.5)",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      zIndex: 1000
-    }}>
+    <div 
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        zIndex: 1000
+      }}
+      onClick={handleBackdropClick}
+    >
       <div style={{
         backgroundColor: "#fff",
         borderRadius: "0.5rem",
