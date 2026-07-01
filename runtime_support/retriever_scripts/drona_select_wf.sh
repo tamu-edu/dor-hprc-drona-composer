568747d250f5a582d29870cca20a9b14bcf94f46
Marinus Pennings <pennings@tamu.edu>
2026-07-01 16:20:28 -0500

    removed the diff messages. Ended up there by mistake

--- runtime_support/retriever_scripts/drona_select_wf.sh
+++ runtime_support/retriever_scripts/drona_select_wf.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 export DRONA_ENV=${DRONA_ENV:-$DRONA_ENV_NAME}
-<<<<<<< HEAD
-#DRONA_ENV:=$DRONA_ENV_NAME}
-=======
->>>>>>> d55d3595b915623995c3a541f69a8508a2236a58
 
 python3 $DRONA_RUNTIME_DIR/retriever_scripts/drona_select_wf.py
 
